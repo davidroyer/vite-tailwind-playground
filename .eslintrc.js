6898a55dee395913517b446f49e40bf0d26c9fce
David Royer <droyer01@gmail.com>
2021-09-01 19:56:36 -0600

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  plugins: ['vue'],
+  // parserOptions: {
+  //   parser: 'babel-eslint'
+  // },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
---
+++ src/library.js
@@ -0,0 +1,2 @@
+export { default as VButton } from './components/VButton.vue';
+export { default as VBlock } from './components/VBlock.vue';
