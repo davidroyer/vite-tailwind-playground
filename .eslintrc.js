e1ce8845db8e834aed855dd36b59c8c7f9c5c54d
David Royer <droyer01@gmail.com>
2021-11-17 12:48:35 -0500

    fix: removes ecmaVersion linting error

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,10 @@ module.exports = {
   ],
 
   plugins: ['vue'],
-  // parserOptions: {
-  //   parser: 'babel-eslint'
-  // },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 6
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
